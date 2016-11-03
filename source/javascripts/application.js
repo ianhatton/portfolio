8998691fe7fa8d29fc13cf464f22a9360e5f8ce5
Ian Hatton <ian@morsedigital.com>
2016-11-03 09:44:30 +0000

    WIP

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,31 +1,13 @@
-var CollapsibleContentClass = require('@ianhatton/vanilla-collapsible-content')
-  , TabbedContentClass = require('@ianhatton/vanilla-tabbed-content');
+var TabbedContentClass = require('@ianhatton/vanilla-tabbed-content');
 
 var HeaderModule = require('./modules/header')
   , LastFmModule = require('./modules/lastfm')
   , SmoothScrollModule = require('./modules/smooth_scroll');
 
-var collapsibleContent
-  , header
+var header
   , lastFm
   , primaryNavsmoothScroll
   , siteIntrosmoothScroll;
-
-// Collapsible Content
-collapsibleContent = document.querySelectorAll('.collapsible-content');
-
-if(collapsibleContent.length > 0) {
-  _.forEach(collapsibleContent, function(cc) {
-    new CollapsibleContentClass({
-      element: cc,
-      bodyContainerClass: 'collapsible-content-body',
-      itemContainerClass: 'collapsible-content-item',
-      toggleContainerClass: 'collapsible-content-toggle',
-      expanded: true
-    });
-  });
-}
-// End Collapsible Content
 
 // Header
 header = HeaderModule('header');
